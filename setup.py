945a79ad383af531f3bdff1b9cdb0fdc5b8712ab
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-14 19:33:08 -0400

    chore: prevent normalization of semver versioning

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with io.open(readme_filename, encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name="google-cloud-service-management",
-    version=version,
+    version=setuptools.sic(version),
     description=description,
     long_description=readme,
     author="Google LLC",
